253e6e44f9b7a649483cfa203feab6f5be3c2fe4
mithon0 <mithonhossen7244@gmail.com>
2023-04-15 17:50:40 +0600

    register with gmail and github

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -3,13 +3,46 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 // import {getAuth} from 'firebase/auth';
 import app from '../firebase/firebase.config';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, GithubAuthProvider } from "firebase/auth"
 
 const auth =getAuth(app);
+
+
+
+
 
 const Register = () => {
     const [error,setError]=useState('');
     const [success,setSuccsess]=useState('');
+// signUp with Google  and git hub
+const googleProvider = new GoogleAuthProvider();
+const gitHuProvider = new GithubAuthProvider();
+const googleHeandler =()=>{
+  signInWithPopup(auth,googleProvider)
+.then(result=>{
+  const user =result.user;
+  console.log(user);
+  setSuccsess('User register Successfully');
+  setError('');
+})
+.catch(error=>{
+  setError(error)
+})
+};
+const gitHubHeandler =()=>{
+    signInWithPopup(auth ,gitHuProvider)
+    .then(result =>{
+      const gitUser =result.user;
+      console.log(gitUser);
+      setSuccsess('User register Successfully');
+      setError('');
+    })
+    .catch(error=>{
+      setError(error)
+    })
+}
+
+    // signUp with email password 
     const submiteHeandler=event=>{ 
         setSuccsess('');
         event.preventDefault();
@@ -40,6 +73,7 @@ const Register = () => {
     // }
     return (
         <div>
+          <h3 className='text-center'>Please register here!</h3>
              <Form onSubmit={submiteHeandler} className='w-50 mx-auto'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
@@ -62,7 +96,12 @@ const Register = () => {
       <p className='text-danger'>{error}</p>
       <p className='text-success'>{success}</p>
     </Form>
-    
+      <h1 className='text-center'>Or</h1>
+    <hr className='w-25 mx-auto p-2' />
+   <div className='w-75 h-25 mx-auto align-center text-center'>
+   <button onClick={googleHeandler} className='w-25 h-25 rounded'> <img className='w-25' src="https://w7.pngwing.com/pngs/63/1016/png-transparent-google-logo-google-logo-g-suite-chrome-text-logo-chrome-thumbnail.png" alt="" /> Sign up With Google!</button>
+   <button onClick={gitHubHeandler} className='w-25 h-25 ms-4 rounded'> <img className='w-25' src="https://w7.pngwing.com/pngs/899/228/png-transparent-git-github-hub-icon-social-network-icon-thumbnail.png" alt="" /> Sign up With GitHub!</button>
+   </div>
         </div>
     );
 };
